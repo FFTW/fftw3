d38b5396624973e2ddc9531587b799eaa13f9daf
Matteo Frigo <athena@fftw.org>
2002-06-16 18:30:32 -0400

    Removed useless file

--- mkdist.sh
+++
@@ -1,6 +0,0 @@
-./configure --enable-maintainer-mode --disable-classic-mode
-make || exit 1
-make distcheck || exit 1
-./configure --enable-maintainer-mode --enable-classic-mode
-make || exit 1
-make distcheck || exit 1
