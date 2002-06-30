a81146d09b6762b1c82b9f998720454245ee5e25
Matteo Frigo <athena@fftw.org>
2002-06-30 14:47:47 -0400

    Forgot file

---
+++ dft/simd/t4.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2002 Matteo Frigo
+ * Copyright (c) 2002 Steven G. Johnson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "codelet.h"
+#include "t4.h"
+
+#if HAVE_SIMD
+int OKP(const ct_desc *d,
+	const R *rio, const R *iio, 
+	int ios, int vs, uint m, int dist)
+{
+     return (1
+	     && ALIGNED(rio)
+	     && ALIGNED(iio)
+	     && (m % VL) == 0
+	     && dist == 1
+	  );
+}
+#endif
