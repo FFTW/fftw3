c267ad079a4ef7cb7a9fdbe4556c89121137be02
Steven G. Johnson <stevenj@fftw.org>
2002-07-21 01:52:54 -0400

    pass identifier in FFTW() through another macro so that the mangled name can itself be a preprocessor symbol

--- kernel/fftw3.h
+++ kernel/fftw3.h
@@ -18,22 +18,23 @@
  *
  */
 
-/* $Id: fftw3.h,v 1.3 2002-07-16 17:55:50 athena Exp $ */
+/* $Id: fftw3.h,v 1.4 2002-07-21 05:52:54 stevenj Exp $ */
 
 /* FFTW installed header file */
 #ifndef __FFTW3_H__
 #define __FFTW3_H__
 
 /* determine precision and name-mangling scheme */
+#define FFTW_MANGLE(prefix, name) prefix ## name
 #if defined(FFTW_SINGLE)
 typedef float fftw_real;
-#define FFTW(name) sfftw_ ## name
+#define FFTW(name) FFTW_MANGLE(sfftw_, name)
 #elif defined(FFTW_LDOUBLE)
 typedef long double fftw_real;
-#define FFTW(name) lfftw_ ## name
+#define FFTW(name) FFTW_MANGLE(lfftw_, name)
 #else
 typedef double fftw_real;
-#define FFTW(name) dfftw_ ## name
+#define FFTW(name) FFTW_MANGLE(dfftw_, name)
 #endif
 
 extern const char *const FFTW(version);
