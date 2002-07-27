b933474c3373bdca65dd9cce3b16272c2b197ee8
Matteo Frigo <athena@fftw.org>
2002-07-27 15:09:40 -0400

    Altivec port

--- simd/simd-altivec.h
+++ simd/simd-altivec.h
@@ -19,25 +19,18 @@
  */
 
 #ifndef FFTW_SINGLE
-#error "Altivec only works in single precision"
+#error "ALTIVEC only works in single precision"
 #endif
 
-#define VL 4            /* SIMD vector length */
+#define VL 2            /* SIMD complex vector length */
 #define ALIGNMENT 16
 
-#if defined(__GNUC__) && (defined(__powerpc__) || defined(__ppc__))
-
-typedef float __attribute__((aligned(16), vector_size(16))) V;
-typedef int Vint __attribute__((vector_size(16)));
-
-#define VADD(a, b) __builtin_altivec_vaddfp(a, b)
-#define VSUB(a, b) __builtin_altivec_vsubfp(a, b)
-#define VFMA(a, b, c) __builtin_altivec_vmaddfp(a, b, c)
-#define VFNMS(a, b, c)__builtin_altivec_vnmsubfp(a, b, c) 
-
-#define LD(var, loc) var = *(const V *)(&(loc))
-#define ST(loc, var) *(V *)(&(loc)) = var
-#define DVK(var, val) static const V var = { val, val, val, val }
+typedef vector float V;
+#define VADD(a, b) vec_add(a, b)
+#define VSUB(a, b) vec_sub(a, b)
+#define VFMA(a, b, c) vec_madd(a, b, c)
+#define VFNMS(a, b, c) vec_nmsub(a, b, c) 
+#define DVK(var, val) V var = (vector float)(val, val, val, val)
 #define LDK(x) x
 
 static inline V VMUL(V a, V b)
@@ -46,38 +39,138 @@ static inline V VMUL(V a, V b)
      return VFMA(a, b, zero);
 }
 
-static inline V VMRGL(V a, V b)
+static inline V VFMS(V a, V b, V c)
 {
-     V ret;
-     __asm__("vmrglw %0, %1, %2" : "=v"(ret) : "v"(a), "v"(b));
-     return ret;
+     return VSUB(VMUL(a, b), c);
 }
 
-static inline V VMRGH(V a, V b)
+/* load lower half */
+static inline V LDL(const float *x) 
 {
-     V ret;
-     __asm__("vmrghw %0, %1, %2" : "=v"(ret) : "v"(a), "v"(b));
-     return ret;
+     V v = vec_ld(0, (float *)x);
+     return vec_perm(v, v, vec_lvsl(0, (float *)x));
 }
 
-
-#define ST4(a, ovs, r0, r1, r2, r3)		\
-{						\
-     R *_b = &(a);				\
-     V _t0, _t1, _t2, _t3;			\
-     _t0 = VMRGH(r0, r2);			\
-     _t1 = VMRGH(r1, r3);			\
-     _t2 = VMRGL(r0, r2);			\
-     _t3 = VMRGL(r1, r3);			\
-						\
-     ST(_b[0 * ovs], VMRGH(_t0,_t1));		\
-     ST(_b[1 * ovs], VMRGL(_t0,_t1));		\
-     ST(_b[2 * ovs], VMRGH(_t2,_t3));		\
-     ST(_b[3 * ovs], VMRGL(_t2,_t3));		\
+static inline V LDH(const float *x) 
+{
+     V v = vec_ld(0, (float *)x);
+     return vec_perm(v, v, vec_lvsl(8, (float *)x));
 }
 
+static inline V LD(const float *x, int ivs) 
+{
+     const vector unsigned int msk = (vector unsigned int)(0, 0, 0xFFFFFFFF, 0xFFFFFFFF);
+     V l = LDL(x);
+     V h = LDH(x + ivs);
+     return vec_sel(l, h, msk);
+}
+
+/* store lower half */
+static inline void STL(float *x, V v)
+{
+     v = vec_perm(v, v, vec_lvsr(0, x));
+     vec_ste(v, 0, x);
+     vec_ste(v, 4, x);
+}
+
+static inline void STH(float *x, V v)
+{
+     v = vec_perm(v, v, vec_lvsr(8, x));
+     vec_ste(v, 0, x);
+     vec_ste(v, 4, x);
+}
+
+static inline void ST(float *x, V v, int ovs) 
+{						
+     STL(x, v);
+     STH(x + ovs, v);
+}
+
+static inline V FLIP_RI(V x)
+{
+     const vector unsigned char perm = 
+	  (vector unsigned char)(4, 5, 6, 7, 0, 1, 2, 3, 
+				 12, 13, 14, 15, 8, 9, 10, 11);
+     return vec_perm(x, x, perm);
+}
+
+static inline V CHS_R(V x)
+{
+     const vector float msk = (vector float)(-1.0, 1.0, -1.0, 1.0);
+     return VMUL(x, msk);
+}
+
+static inline V VBYI(V x)
+{
+     return CHS_R(FLIP_RI(x));
+}
+
+static inline V VFMAI(V b, V c)
+{
+     const vector float msk = (vector float)(-1.0, 1.0, -1.0, 1.0);
+     return VFMA(FLIP_RI(b), msk, c);
+}
+
+static inline V VFNMSI(V b, V c)
+{
+     const vector float msk = (vector float)(-1.0, 1.0, -1.0, 1.0);
+     return VFNMS(FLIP_RI(b), msk, c);
+}
+
+#define FAST_AND_BLOATED_TWIDDLES
+
+#ifdef FAST_AND_BLOATED_TWIDDLES
+
+/* avoid twiddle shuffling by storing twiddles twice in the
+   proper format */
+
+#define VTW(x) 								\
+  {TW_COS, 0, x}, {TW_COS, 0, x}, {TW_COS, 1, x}, {TW_COS, 1, x},	\
+  {TW_SIN, 0, -x}, {TW_SIN, 0, x}, {TW_SIN, 1, -x}, {TW_SIN, 1, x}
+#define TWVL (2 * VL)
+
+static inline V BYTW(const R *t, V sr)
+{
+     const V *twp = (const V *)t;
+     V si = FLIP_RI(sr);
+     V tr = twp[0], ti = twp[1];
+     return VFMA(ti, si, VMUL(tr, sr));
+}
+
+static inline V BYTWJ(const R *t, V sr)
+{
+     const V *twp = (const V *)t;
+     V si = FLIP_RI(sr);
+     V tr = twp[0], ti = twp[1];
+     return VFNMS(ti, si, VMUL(tr, sr));
+}
+
+#else /* FAST_AND_BLOATED_TWIDDLES */
+
+#define VTW(x) 								\
+  {TW_COS, 0, x}, {TW_COS, 1, x}, {TW_SIN, 0, x}, {TW_SIN, 1, x}
+#define TWVL (VL)
+
+static inline V BYTW(const R *t, V sr)
+{
+     const V *twp = (const V *)t;
+     V si = FLIP_RI(sr);
+     V tx = twp[0];
+     V tr = vec_mergeh(tx, tx);
+     V ti = vec_mergel(tx, tx);
+     return VFMA(CHS_R(ti), si, VMUL(tr, sr));
+}
+
+static inline V BYTWJ(const R *t, V sr)
+{
+     const V *twp = (const V *)t;
+     V si = FLIP_RI(sr);
+     V tx = twp[0];
+     V tr = vec_mergeh(tx, tx);
+     V ti = vec_mergel(tx, tx);
+     return VFNMS(CHS_R(ti), si, VMUL(tr, sr));
+}
 #endif
 
-#define VTW(op, x) {op, 0, x}, {op, 1, x}, {op, 2, x}, {op, 3, x}
+#define RIGHT_CPU() 1
 
-#define RIGHT_CPU() 1  /* TODO */ 
