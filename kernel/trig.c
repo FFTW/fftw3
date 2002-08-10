9b354635204711389328f487a058a54604d58e0a
Matteo Frigo <athena@fftw.org>
2002-08-10 14:41:04 -0400

    Improved trig scheme

--- kernel/trig.c
+++ kernel/trig.c
@@ -18,7 +18,7 @@
  *
  */
 
-/* $Id: trig.c,v 1.6 2002-08-10 00:49:32 athena Exp $ */
+/* $Id: trig.c,v 1.7 2002-08-10 18:41:04 athena Exp $ */
 
 /* trigonometric functions */
 #include "ifftw.h"
@@ -40,14 +40,58 @@
 #  define KTRIG(x) (x)
 #endif
 
+#ifdef REALLY_ACCURATE
+extern double fma (double X, double Y, double Z);
+
+/* compute *x + *dx = a * b exactly */
+static void dbmul(double a, double b, double *x, double *dx)
+{
+     *x = a * b; 
+     *dx = fma(a, b, -*x);
+}
+
+/* compute *x + *dx = a / b accurately */
+static void dbdiv(double a, double b, double *x, double *dx)
+{
+     *x = a / b;
+     *dx = fma(-*x, b, a) / b;
+}
+
+static double by2pi(double m, double n)
+{
+     /* 2 PI rounded to IEEE-754 double precision */
+     static const double rpi2 =
+	  6.28318530717958623199592693708837032318115234375;
+     /* 2 PI - rpi2 */
+     static const double rpi2r = 
+	  2.44929359829470635445213186455000211641949889184e-16;
+
+     double x, y, z, dx, dy, dz;
+
+     dbmul(rpi2, m, &x, &dx);      /* x + dx = rpi2 * m, exactly */
+     dbmul(rpi2r, m, &y, &dy);     /* x + dx = rpi2r * m, exactly */
+
+     /* x + dx ~ 2 PI * m, we lose roundoff in dx */
+     y += dx;
+     dx = y + dy;
+
+     dbdiv(x, n, &y, &dy);      /* y + dy = x / n */
+     dbdiv(dx, n, &z, &dz);     /* z + dz = dx / n */
+
+     return ((z + dy) + dz) + y;
+}
+#else
+
+static const trigreal K2PI =
+    KTRIG(6.2831853071795864769252867665590057683943388);
+#define by2pi(m, n) ((K2PI * (m)) / (n))
+
+#endif
+
 /*
  * Improve accuracy by reducing x to range [0..1/8]
  * before multiplication by 2 * PI.
  */
-
-static const trigreal K2PI =
-    KTRIG(6.2831853071795864769252867665590057683943388);
-
 
 static trigreal sin2pi0(trigreal m, trigreal n);
 static trigreal cos2pi0(trigreal m, trigreal n)
@@ -56,7 +100,7 @@ static trigreal cos2pi0(trigreal m, trigreal n)
      if (m > n * 0.5) return cos2pi0(n - m, n);
      if (m > n * 0.25) return -sin2pi0(m - n * 0.25, n);
      if (m > n * 0.125) return sin2pi0(n * 0.25 - m, n);
-     return COS(K2PI * (m / n));
+     return COS(by2pi(m, n));
 }
 
 static trigreal sin2pi0(trigreal m, trigreal n)
@@ -65,7 +109,7 @@ static trigreal sin2pi0(trigreal m, trigreal n)
      if (m > n * 0.5) return -sin2pi0(n - m, n);
      if (m > n * 0.25) return cos2pi0(m - n * 0.25, n);
      if (m > n * 0.125) return cos2pi0(n * 0.25 - m, n);
-     return SIN(K2PI * (m / n));
+     return SIN(by2pi(m, n));
 }
 
 trigreal cos2pi(int m, uint n)
@@ -80,6 +124,7 @@ trigreal sin2pi(int m, uint n)
 
 trigreal tan2pi(int m, uint n)
 {
+     trigreal dm = m, dn = n;
      /* unimplemented, unused */
-     return TAN(K2PI * ((trigreal)m / (trigreal)n));
+     return TAN(by2pi(dm, dn));
 }
--- tests/trigtest.c
+++ tests/trigtest.c
@@ -8,27 +8,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef USE_PARI
 #define USE_PARI 1
+#endif
 
 typedef double trigreal;
 #  define COS cos
 #  define SIN sin
 #  define TAN tan
 #  define KTRIG(x) (x)
+static const trigreal MYK2PI =
+    KTRIG(6.2831853071795864769252867665590057683943388);
+
+/**************************************************************/
+/* copy-and-paste for kernel/trig.c */
+
+#ifdef REALLY_ACCURATE
+extern double fma (double X, double Y, double Z);
+
+/* compute *x + *dx = a * b exactly */
+static void dbmul(double a, double b, double *x, double *dx)
+{
+     *x = a * b; 
+     *dx = fma(a, b, -*x);
+}
+
+/* compute *x + *dx = a / b accurately */
+static void dbdiv(double a, double b, double *x, double *dx)
+{
+     *x = a / b;
+     *dx = fma(-*x, b, a) / b;
+}
+
+static double by2pi(double m, double n)
+{
+     /* 2 PI rounded to IEEE-754 double precision */
+     static const double rpi2 =
+	  6.28318530717958623199592693708837032318115234375;
+     /* 2 PI - rpi2 */
+     static const double rpi2r = 
+	  2.44929359829470635445213186455000211641949889184e-16;
+
+     double x, y, z, dx, dy, dz;
+
+     dbmul(rpi2, m, &x, &dx);      /* x + dx = rpi2 * m, exactly */
+     dbmul(rpi2r, m, &y, &dy);     /* x + dx = rpi2r * m, exactly */
+
+     /* x + dx ~ 2 PI * m, we lose roundoff in dx */
+     y += dx;
+     dx = y + dy;
+
+     dbdiv(x, n, &y, &dy);      /* y + dy = x / n */
+     dbdiv(dx, n, &z, &dz);     /* z + dz = dx / n */
+
+     return ((z + dy) + dz) + y;
+}
+#else
 
 static const trigreal K2PI =
     KTRIG(6.2831853071795864769252867665590057683943388);
+#define by2pi(m, n) ((K2PI * (m)) / (n))
 
-trigreal naive_sin2pi(int m, uint n)
-{
-     return SIN(K2PI * ((trigreal) m / (trigreal) n));
-}
+#endif
 
-trigreal naive_cos2pi(int m, uint n)
-{
-     return COS(K2PI * ((trigreal) m / (trigreal) n));
-}
-
+/*
+ * Improve accuracy by reducing x to range [0..1/8]
+ * before multiplication by 2 * PI.
+ */
 
 static trigreal sin2pi0(trigreal m, trigreal n);
 static trigreal cos2pi0(trigreal m, trigreal n)
@@ -37,7 +83,7 @@ static trigreal cos2pi0(trigreal m, trigreal n)
      if (m > n * 0.5) return cos2pi0(n - m, n);
      if (m > n * 0.25) return -sin2pi0(m - n * 0.25, n);
      if (m > n * 0.125) return sin2pi0(n * 0.25 - m, n);
-     return COS(K2PI * (m / n));
+     return COS(by2pi(m, n));
 }
 
 static trigreal sin2pi0(trigreal m, trigreal n)
@@ -46,7 +92,7 @@ static trigreal sin2pi0(trigreal m, trigreal n)
      if (m > n * 0.5) return -sin2pi0(n - m, n);
      if (m > n * 0.25) return cos2pi0(m - n * 0.25, n);
      if (m > n * 0.125) return cos2pi0(n * 0.25 - m, n);
-     return SIN(K2PI * (m / n));
+     return SIN(by2pi(m, n));
 }
 
 trigreal cos2pi(int m, uint n)
@@ -58,6 +104,26 @@ trigreal sin2pi(int m, uint n)
 {
      return sin2pi0((trigreal)m, (trigreal)n);
 }
+
+trigreal tan2pi(int m, uint n)
+{
+     trigreal dm = m, dn = n;
+     /* unimplemented, unused */
+     return TAN(by2pi(dm, dn));
+}
+
+/**************************************************************/
+/* test code */
+trigreal naive_sin2pi(int m, uint n)
+{
+     return SIN(MYK2PI * ((trigreal) m / (trigreal) n));
+}
+
+trigreal naive_cos2pi(int m, uint n)
+{
+     return COS(MYK2PI * ((trigreal) m / (trigreal) n));
+}
+
 
 #if USE_PARI
 #include <pari/pari.h>
