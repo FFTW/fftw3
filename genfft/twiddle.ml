c5ef4a2ddba0963a8c9a388edf050c5ee2fbbb00
Matteo Frigo <athena@fftw.org>
2002-08-17 10:47:59 -0400

    New twiddle policy (disabled for now)

--- genfft/twiddle.ml
+++ genfft/twiddle.ml
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *)
-(* $Id: twiddle.ml,v 1.7 2002-08-17 00:27:10 athena Exp $ *)
+(* $Id: twiddle.ml,v 1.8 2002-08-17 14:47:59 athena Exp $ *)
 
 (* policies for loading/computing twiddle factors *)
 open Complex
@@ -107,6 +107,63 @@ let load_reim sign w i =
 
 (* various policies for computing/loading twiddle factors *)
 
+(* addition chain: given a set s of numbers to be stored, and the cost
+   of multiplications and squarings, compute the optimal solution
+   via dynamic programming 
+ *)
+   
+let addition_chain n s load cost_load cost_mul cost_sq =
+  let infty = 100000 in
+  let solution = Array.init n (fun i () -> failwith "addition_chain") in
+  let solution = Array.init n (fun i () -> failwith (string_of_int i)) in
+  let costs = Array.init n (fun i -> infty) in
+  let changed = ref false in
+  let update_one i j k how =
+    if (k >= 0 && k < n) then
+      let c = if i = j then cost_sq else cost_mul in
+      let d = c + costs.(i) + costs.(j) in
+      if (d < costs.(k)) then
+	begin
+	  costs.(k) <- d;
+	  solution.(k) <- how;
+	  changed := true;
+	end
+  in
+  let update_all () = 
+    begin
+      changed := false;
+      for i = 0 to n - 1 do
+	for j = 0 to n - 1 do
+	  begin
+	    update_one i j (i + j)
+	      (fun () -> Complex.times (solution.(i) ()) (solution.(j) ()));
+	    update_one i j (i - j)
+	      (fun () -> 
+		Complex.times (solution.(i) ()) 
+		  (Complex.conj (solution.(j) ())));
+	  end;
+	done
+      done
+    end;
+  in
+  let init () =
+    begin
+      costs.(0) <- 0; (* always free *)
+      solution.(0) <- (fun () -> Complex.one);
+      List.iter2 (fun k i -> 
+	costs.(k) <- cost_load;
+	solution.(k) <- (fun () -> load i))
+	s (iota (List.length s));
+    end
+  in begin
+    init ();
+    changed := true;
+    while !changed do update_all () done;
+    fun i -> solution.(i) ();
+  end
+  
+   
+
 (* load all twiddle factors *)
 let twiddle_policy_load_all =
   let bytwiddle n sign w f i =
@@ -116,6 +173,43 @@ let twiddle_policy_load_all =
       Complex.times (load_reim sign w (i - 1)) (f i)
   and twidlen n = 2 * (n - 1)
   and twdesc r = [(TW_FULL, 0, r);(TW_NEXT, 1, 0)]
+  in bytwiddle, twidlen, twdesc
+
+let twiddle_policy_addition_chain t =
+  let load_set n = match (n, t) with
+    | (4, 1) -> [1;] (* 20 *)
+    | (4, 2) -> [1;3;] (* 8 *)
+    | (4, _) -> [1;2;3;] (* 3 *)
+    | (8, 1) -> [1;] (* 122 *)
+    | (8, 2) -> [2;3;] (* 43 *)
+    | (8, 3) -> [1;3;4;] (* 31 *)
+    | (8, 4) -> [1;2;3;5;] (* 24 *)
+    | (8, 5) -> [1;2;3;5;7;] (* 17 *)
+    | (16, 1) -> [1;] (* 574 *)
+    | (16, 2) -> [3;5;] (* 162 *)
+    | (16, 3) -> [4;5;7;] (* 108 *)
+    | (16, 4) -> [1;3;7;12;] (* 86 *)
+    | (16, 5) -> [1;3;4;7;12;] (* 77 *)
+    | (32, 1) -> [1;] (* 2454 *)
+    | (32, 2) -> [5;6;] (* 524 *)
+    | (32, 3) -> [7;8;10;] (* 343 *)
+    | (32, 4) -> [6;11;14;15;] (* 271 *)
+    | (32, 5) -> [5;8;12;14;15;] (* 228 *)
+    | (64, 1) -> [1;] (* 10086 *)
+    | (64, 2) -> [7;9;] (* 1614 *)
+    | (64, 3) -> [11;12;19;] (* 975 *)
+    | (64, 4) -> [11;17;20;21;] (* 756 *)
+    | (64, 5) -> [3;17;18;28;30;] (* 644 *)
+    | _ -> failwith "unknown addition chain"
+  in
+  let bytwiddle n sign w f =
+    let g = addition_chain n (load_set n) (load_reim sign w) 1 6 4 in
+    fun i -> Complex.times (g i) (f i)    
+  and twidlen n = 2 * List.length (load_set n)
+  and twdesc r = 
+    List.flatten (List.map (fun i -> [(TW_COS, 0, i); (TW_SIN, 0, i)])
+		    (load_set r))
+    @ [(TW_NEXT, 1, 0)]
   in bytwiddle, twidlen, twdesc
 
 (*
@@ -268,6 +362,7 @@ let current_twiddle_policy = ref twiddle_policy_load_all
 let twiddle_policy () = !current_twiddle_policy
 
 let set_policy x = Arg.Unit (fun () -> current_twiddle_policy := x)
+let set_policy_int x = Arg.Int (fun i -> current_twiddle_policy := x i)
 
 let undocumented = " Undocumented twiddle policy"
 
@@ -281,4 +376,6 @@ let speclist = [
   "-twiddle-square2", set_policy twiddle_policy_square2, undocumented;
   "-twiddle-square3", set_policy twiddle_policy_square3, undocumented;
   "-twiddle-ooura", set_policy twiddle_policy_ooura, undocumented;
+  "-twiddle-addchain", set_policy_int twiddle_policy_addition_chain, 
+  undocumented;
 ] 
---
+++ support/addchain.c
@@ -0,0 +1,151 @@
+/* addition-chain optimizer */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int verbose;
+static int mulcost = 6;
+static int ldcost = 1;
+static int sqcost = 4;
+#define INFTY 100000
+
+static int *answer;
+static int best_so_far;
+
+static void print_answer(int n, int t)
+{
+     int i;
+     printf("| (%d, %d) -> [", n, t);
+     for (i = 0; i < t; ++i)
+	  printf("%d;", answer[i]);
+     printf("] (* %d *)\n", best_so_far);
+}
+
+#define DO(i, j, k, cst)			\
+if (k >= 0 && k < n) {				\
+     int c = A[i] + A[j] + cst;			\
+     if (c < A[k]) {				\
+	  A[k] = c;				\
+	  changed = 1;				\
+     }						\
+}
+
+static int optimize(int n, int *A)
+{
+     int i, j, k, changed, cst;
+
+     do {
+	  changed = 0;
+	  for (i = 0; i < n; ++i) {
+	       k = i + i;
+	       DO(i, i, k, sqcost);
+	  }
+
+	  for (i = 0; i < n; ++i)
+	       for (j = 0; j < n; ++j) {
+		    k = i + j;
+		    DO(i, j, k, mulcost);
+		    k = i - j;
+		    DO(i, j, k, mulcost);
+	       }
+
+     } while (changed);
+
+     cst = 0;
+     for (i = 0; i < n; ++i)
+	  cst += A[i];
+     return cst;
+}
+
+static void search(int n, int t, int *A, int *B, int depth)
+{
+     if (depth == 0) {
+	  int i, tc;
+	  for (i = 0; i < n; ++i)
+	       A[i] = INFTY;
+	  A[0] = 0;		/* always free */
+	  for (i = 1; i <= t; ++i)
+	       A[B[-i]] = ldcost;
+
+	  tc = optimize(n, A);
+	  if (tc < best_so_far) {
+	       best_so_far = tc;
+	       for (i = 1; i <= t; ++i)
+		    answer[t - i] = B[-i];
+	       if (verbose)
+		    print_answer(n, t);
+	  }
+     } else {
+	  for (B[0] = B[-1] + 1; B[0] < n; ++B[0])
+	       search(n, t, A, B + 1, depth - 1);
+     }
+}
+
+static void doit(int n, int t)
+{
+     int *A;
+     int *B;
+
+     A = malloc(n * sizeof(int));
+     B = malloc((t + 1) * sizeof(int));
+     answer = malloc(t * sizeof(int));
+
+     B[0] = 0;
+     best_so_far = INFTY;
+     search(n, t, A, B + 1, t);
+
+     print_answer(n, t);
+
+     free(A); free(B); free(answer);
+}
+
+int main(int argc, char *argv[])
+{
+     int n = 32;
+     int t = 3;
+     int all;
+     int ch;
+
+     verbose = 0;
+     all = 0;
+     while ((ch = getopt(argc, argv, "n:t:m:l:s:va")) != -1) {
+	  switch (ch) {
+	  case 'n':
+	       n = atoi(optarg);
+	       break;
+	  case 't':
+	       t = atoi(optarg);
+	       break;
+	  case 'm':
+	       mulcost = atoi(optarg);
+	       break;
+	  case 'l':
+	       ldcost = atoi(optarg);
+	       break;
+	  case 's':
+	       sqcost = atoi(optarg);
+	       break;
+	  case 'v':
+	       ++verbose;
+	       break;
+	  case 'a':
+	       ++all;
+	       break;
+	  case '?':
+	       fprintf(stderr, "use the source\n");
+	       exit(1);
+	  }
+     }
+
+     if (all) {
+	  for (n = 4; n <= 64; n *= 2) {
+	       int n1 = n - 1; if (n1 > 5) n1 = 5;
+	       for (t = 1; t <= n1; ++t)
+		    doit(n, t);
+	  }
+     } else {
+	  doit(n, t);
+     }
+
+     return 0;
+}
