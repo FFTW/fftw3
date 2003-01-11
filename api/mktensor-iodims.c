c70d4ae24f18ed8573746982ded357d7a66e45d7
Steven G. Johnson <stevenj@fftw.org>
2003-01-11 17:57:29 -0500

    whoops

---
+++ api/mktensor-iodims.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2002 Matteo Frigo
+ * Copyright (c) 2002 Steven G. Johnson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "api.h"
+
+tensor *X(mktensor_iodims)(unsigned int rank, const X(iodim) *dims)
+{
+     unsigned int i;
+     tensor *x = X(mktensor)(rank);
+     
+     if (FINITE_RNK(rank)) {
+	  for (i = 0; i < rank; ++i) {
+	       x->dims[i].n = dims[i].n;
+	       x->dims[i].is = dims[i].is;
+	       x->dims[i].os = dims[i].os;
+	  }
+     }
+     return x;
+}
