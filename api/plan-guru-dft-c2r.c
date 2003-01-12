f50b2491505035d8da53cdc2f807f777ab7f2fa2
Steven G. Johnson <stevenj@fftw.org>
2003-01-12 12:19:53 -0500

    whoops

---
+++ api/plan-guru-dft-c2r.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2002 Matteo Frigo
+ * Copyright (c) 2002 Steven G. Johnson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "api.h"
+#include "rdft.h"
+
+X(plan) X(plan_guru_dft_c2r)(unsigned int rank, const X(iodim) *dims,
+			     unsigned int howmany_rank,
+			     const X(iodim) *howmany_dims,
+			     R *ri, R *ii, R *out,
+			     unsigned int flags)
+{
+     flags |= FFTW_DESTROY_INPUT;
+     return X(mkapiplan)(
+	  flags,
+	  X(mkproblem_rdft2_d)(X(mktensor_iodims)(rank, dims),
+			       X(mktensor_iodims)(howmany_rank, howmany_dims),
+			       out, ri, ii, HC2R));
+}
---
+++ api/plan-guru-dft.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2002 Matteo Frigo
+ * Copyright (c) 2002 Steven G. Johnson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "api.h"
+#include "dft.h"
+
+X(plan) X(plan_guru_dft)(unsigned int rank, const X(iodim) *dims,
+			 unsigned int howmany_rank,
+			 const X(iodim) *howmany_dims,
+			 R *ri, R *ii, R *ro, R *io,
+			 unsigned int flags)
+{
+     return X(mkapiplan)(
+	  flags,
+	  X(mkproblem_dft_d)(X(mktensor_iodims)(rank, dims),
+			     X(mktensor_iodims)(howmany_rank, howmany_dims),
+			     ri, ii, ro, io));
+}
