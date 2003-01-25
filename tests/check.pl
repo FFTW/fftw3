4cf6b31bc1d606a85ebe86b81538440c32ba16d4
Matteo Frigo <athena@fftw.org>
2003-01-25 11:48:19 -0500

    New tests, added make check

--- tests/check.pl
+++ tests/check.pl
@@ -10,6 +10,12 @@ $patient = 0;
 $estimate = 0;
 $nowisdom = 0;
 $rounds=0;
+$maxsize = 60000;
+$maxcount = 100;
+$do_1d = 0;
+$do_2d = 0;
+$do_random = 0;
+$keepgoing = 0;
 
 sub make_options {
     my $options = $default_options;
@@ -32,6 +38,7 @@ sub do_problem {
 	    if $verbose;
 	if (system("$program $options --verify $problem") != 0) {
 	    print "FAILED: $problem does not verify\n";
+	    exit 1 unless $keepgoing;
 	    @failures = ($problem, @failures);
 	}
     } else {
@@ -39,6 +46,7 @@ sub do_problem {
 	    if $verbose;
 	if (`$program $options --can-do $problem` ne "#f\n") {
 	    print "FAILED: $problem is not undoable\n";
+	    exit 1 unless $keepgoing;
 	    @failures = ($problem, @failures);
 	}
     }
@@ -72,11 +80,51 @@ sub small_1d {
 sub small_2d {
     do_size ("0x0", 0);
     for ($i = 1; $i <= 100; ++$i) {
-	my $ub = 3000/$i;
+	my $ub = 900/$i;
 	$ub = 100 if $ub > 100;
 	for ($j = 1; $j <= $ub; ++$j) {
 	    do_size ("${i}x${j}", 1);
 	}
+    }
+}
+
+sub rand_small_factors {
+    my $l = shift;
+    my $n = 1;
+    my $maxfactor = 13;
+    my $f = int(rand($maxfactor) + 1);
+    while ($n * $f < $l) {
+	$n *= $f;
+	$f = int(rand($maxfactor) + 1);
+    };
+    return $n;
+}
+
+# way too complicated...
+sub one_random_test {
+    my $q = int(2 + rand($maxsize));
+    my $rnk = int(1 + rand(3));
+    my $g = int(2 + exp(log($q) / $rnk));
+    my $first = 1;
+    my $sz = "";
+
+    while ($q > 1) {
+	my $r = rand_small_factors(int(rand($g) + 10));
+	if ($r > 1) {
+	    $sz = "${sz}x" if (!$first);
+	    $first = 0;
+	    $sz = "${sz}${r}";
+	    $q = int($q / $r);
+	    if ($g > $q) { $g = $q; }
+	}
+    }
+    do_size($sz, 1)
+}
+
+sub random_tests {
+    my $i;
+    for ($i = 0; $i < $maxcount; ++$i) {
+	&one_random_test;
     }
 }
 
@@ -94,7 +142,20 @@ sub parse_arguments (@)
 	elsif ($arglist[0] eq '--patient') { ++$patient; }
 	elsif ($arglist[0] eq '--estimate') { ++$estimate; }
 	elsif ($arglist[0] eq '--nowisdom') { ++$nowisdom; }
+	elsif ($arglist[0] eq '-k') { ++$keepgoing; }
+	elsif ($arglist[0] eq '--keep-going') { ++$keepgoing; }
 	elsif ($arglist[0] =~ /^--verify-rounds=(.+)$/) { $rounds = $1; }
+	elsif ($arglist[0] =~ /^--count=(.+)$/) { $count = $1; }
+	elsif ($arglist[0] =~ /^-c=(.+)$/) { $count = $1; }
+	
+	elsif ($arglist[0] eq '-1d') { ++$do_1d; }
+	elsif ($arglist[0] eq '-2d') { ++$do_2d; }
+	elsif ($arglist[0] eq '-r') { ++$do_random; }
+	elsif ($arglist[0] eq '--random') { ++$do_random; }
+	elsif ($arglist[0] eq '-a') { 
+	    ++$do_1d; ++$do_2d; ++$do_random; 
+	}
+
 	else { $program=$arglist[0]; }
 	shift (@arglist);
     }
@@ -103,6 +164,8 @@ sub parse_arguments (@)
 # MAIN PROGRAM:
 
 &parse_arguments (@ARGV);
-&small_1d;
-&small_2d;
+
+&small_1d if $do_1d;
+&small_2d if $do_2d;
+&random_tests if $do_random;
 
