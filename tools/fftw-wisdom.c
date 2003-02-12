d643ece55b08510928523882ac2213361d1eaf43
Steven G. Johnson <stevenj@fftw.org>
2003-02-11 19:07:12 -0500

    added -V

--- tools/fftw-wisdom.c
+++ tools/fftw-wisdom.c
@@ -53,6 +53,7 @@ static struct option long_options[] =
 {
   {"help", no_argument, 0, 'h'},
   {"verbose", optional_argument, 0, 'v'},
+  {"version", optional_argument, 0, 'V'},
 
   {"canonical", no_argument, 0, 'c'},
 
@@ -121,6 +122,30 @@ int bench_main(int argc, char *argv[])
 			verbose = atoi(optarg);
 		   else
 			++verbose;
+		   break;
+		   
+	      case 'V':
+		   printf("fftw-wisdom tool for FFTW version " VERSION ".\n");
+		   printf(
+"\n"
+"Copyright (c) 2002 Matteo Frigo\n"
+"Copyright (c) 2002 Steven G. Johnson\n"
+"\n"
+"This program is free software; you can redistribute it and/or modify\n"
+"it under the terms of the GNU General Public License as published by\n"
+"the Free Software Foundation; either version 2 of the License, or\n"
+"(at your option) any later version.\n"
+"\n"
+"This program is distributed in the hope that it will be useful,\n"
+"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"GNU General Public License for more details.\n"
+"\n"
+"You should have received a copy of the GNU General Public License\n"
+"along with this program; if not, write to the Free Software\n"
+"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n"
+			);
+
 		   break;
 		   
 	      case 'c':
