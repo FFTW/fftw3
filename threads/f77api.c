08b64e3b15f7ad163677a348ba8d0a1a62720b07
Steven G. Johnson <stevenj@fftw.org>
2003-03-06 13:21:03 -0500

    pay attention to WINDOWS_F77_MANGLING

--- threads/f77api.c
+++ threads/f77api.c
@@ -22,18 +22,41 @@
 
 /* if F77_FUNC is not defined, then we don't know how to mangle identifiers
    for the Fortran linker, and we must omit the f77 API. */
-#ifdef F77_FUNC
+#if defined(F77_FUNC) || defined(WINDOWS_F77_MANGLING)
 
 #include "x77.h"
 
-#define F77x(a, A) F77_FUNC(a, A)
 #define F77(a, A) F77x(x77(a), X77(A))
-#include "f77funcs.c"
+
+#ifndef WINDOWS_F77_MANGLING
+
+#if defined(F77_FUNC)
+#  define F77x(a, A) F77_FUNC(a, A)
+#  include "f77funcs.c"
+#endif
 
 #if defined(F77_FUNC_) && !defined(F77_FUNC_EQUIV)
 #  undef F77x
 #  define F77x(a, A) F77_FUNC_(a, A)
 #  include "f77funcs.c"
 #endif
+
+#else /* WINDOWS_F77_MANGLING */
+
+#  define WINDOWS_F77_FUNC(a, A) a ## __
+#  define F77x(a, A) WINDOWS_F77_FUNC(a, A)
+#  include "f77funcs.c"
+
+#  undef WINDOWS_F77_FUNC
+#  if defined(__GNUC__)
+#    define WINDOWS_F77_FUNC(a, A) __attribute__((stdcall)) A
+#  elif defined(_MSC_VER) || defined(_ICC) || defined(_STDCALL_SUPPORTED)
+#    define WINDOWS_F77_FUNC(a, A) __stdcall A
+#  else
+#    define WINDOWS_F77_FUNC(a, A) A /* oh well */
+#  endif
+#  include "f77funcs.c"
+
+#endif /* WINDOWS_F77_MANGLING */
 
 #endif				/* F77_FUNC */
