26c7e51b101004fe0cc9c2a5f90c732fd7bcf2b2
Steven G. Johnson <stevenj@fftw.org>
2003-09-23 23:27:29 -0400

    updated documentation for mach_absolute_time

--- kernel/cycle.h
+++ kernel/cycle.h
@@ -23,7 +23,7 @@
  *
  */
 
-/* $Id: cycle.h,v 1.39 2003-09-24 03:25:52 stevenj Exp $ */
+/* $Id: cycle.h,v 1.40 2003-09-24 03:27:29 stevenj Exp $ */
 
 /* machine-dependent cycle counters code. Needs to be inlined. */
 
@@ -58,13 +58,13 @@
 
    AC_C_INLINE
    AC_HEADER_TIME
-   AC_CHECK_HEADERS([sys/time.h c_asm.h intrinsics.h])
+   AC_CHECK_HEADERS([sys/time.h c_asm.h intrinsics.h mach/mach_time.h])
 
    AC_CHECK_TYPE([hrtime_t],[AC_DEFINE(HAVE_HRTIME_T, 1, [Define to 1 if hrtime_t is defined in <sys/time.h>])],,[#if HAVE_SYS_TIME_H
 #include <sys/time.h>
 #endif])
 
-   AC_CHECK_FUNCS([gethrtime read_real_time time_base_to_time clock_gettime])
+   AC_CHECK_FUNCS([gethrtime read_real_time time_base_to_time clock_gettime mach_absolute_time])
 
    dnl Cray UNICOS _rtc() (real-time clock) intrinsic
    AC_MSG_CHECKING([for _rtc intrinsic])
