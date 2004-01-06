4f45958b21e10e4b0ad128e4a36b3b1c456e7a22
Steven G. Johnson <stevenj@fftw.org>
2004-01-06 00:32:58 -0500

    added padded real rader

--- rdft/dht-rader.c
+++ rdft/dht-rader.c
@@ -28,6 +28,7 @@
 
 typedef struct {
      solver super;
+     int pad;
 } S;
 
 typedef struct {
@@ -35,7 +36,7 @@ typedef struct {
 
      plan *cld1, *cld2;
      R *omega;
-     int n, g, ginv;
+     int n, npad, g, ginv;
      int is, os;
      plan *cld_omega;
 } P;
@@ -48,18 +49,19 @@ static rader_tl *omegas = 0;
    purely in terms of R2HC transforms, as opposed to R2HC followed by H2RC.
    This requires a few more operations, but allows us to share the same
    plan/codelets for both Rader children. */
-#define R2HC_ONLY_CONV 1
+#define R2HC_ONLY_CONV 0
 
 static void apply(const plan *ego_, R *I, R *O)
 {
      const P *ego = (const P *) ego_;
      int r = ego->n;
+     int rpad = ego->npad; /* == r - 1 for unpadded Rader; always even */
      int is = ego->is, os;
      int k, gpower, g;
      R *buf, *omega;
      R r0;
 
-     buf = (R *) MALLOC(sizeof(R) * (r - 1), BUFFERS);
+     buf = (R *) MALLOC(sizeof(R) * rpad, BUFFERS);
 
      /* First, permute the input, storing in buf: */
      g = ego->g; 
@@ -68,73 +70,92 @@ static void apply(const plan *ego_, R *I, R *O)
      }
      /* gpower == g^(r-1) mod r == 1 */;
 
+     A(r - 1 <= rpad);
+     for (k = r - 1; k < rpad; ++k)
+	  buf[k] = 0;
+
      os = ego->os;
 
-     /* compute RDFT of buf, storing in output (except DC): */
+     /* compute RDFT of buf, storing in buf (i.e., in-place): */
      {
 	    plan_rdft *cld = (plan_rdft *) ego->cld1;
-	    cld->apply((plan *) cld, buf, O + os);
+	    cld->apply((plan *) cld, buf, buf);
      }
 
      /* set output DC component: */
-     O[0] = (r0 = I[0]) + O[os];
+     O[0] = (r0 = I[0]) + buf[0];
 
      /* now, multiply by omega: */
      omega = ego->omega;
 
-     O[(0 + 1) * os] *= omega[0];
+     buf[0] *= omega[0];
 #if R2HC_ONLY_CONV
-     for (k = 1; k < (r - 1)/2; ++k) {
+     for (k = 1; k < rpad/2; ++k) {
 	  E rB, iB, rW, iW, a, b;
 	  rW = omega[k];
-	  iW = omega[(r-1) - k];
-	  rB = O[(k + 1) * os];
-	  iB = O[((r-1) - k + 1) * os];
+	  iW = omega[rpad - k];
+	  rB = buf[k];
+	  iB = buf[rpad - k];
 	  a = rW * rB - iW * iB;
 	  b = rW * iB + iW * rB;
-	  O[(k + 1) * os] = a + b;
-	  O[((r-1) - k + 1) * os] = a - b;
+	  buf[k] = a + b;
+	  buf[rpad - k] = a - b;
      }
 #else
-     for (k = 1; k < (r - 1)/2; ++k) {
+     for (k = 1; k < rpad/2; ++k) {
 	  E rB, iB, rW, iW;
 	  rW = omega[k];
-	  iW = omega[(r-1) - k];
-	  rB = O[(k + 1) * os];
-	  iB = O[((r-1) - k + 1) * os];
-	  O[(k + 1) * os] = rW * rB - iW * iB;
-	  O[((r-1) - k + 1) * os] = rW * iB + iW * rB;
+	  iW = omega[rpad - k];
+	  rB = buf[k];
+	  iB = buf[rpad - k];
+	  buf[k] = rW * rB - iW * iB;
+	  buf[rpad - k] = rW * iB + iW * rB;
      }
 #endif
      /* Nyquist component: */
-     O[(k + 1) * os] *= omega[k]; /* k == (r-1)/2, since r-1 is even */
+     A(k + k == rpad); /* since rpad is even */
+     buf[k] *= omega[k];
      
      /* this will add input[0] to all of the outputs after the ifft */
-     O[os] += r0;
+     buf[0] += (rpad == r - 1) ? r0 : (r0 * 0.5);
 
      /* inverse FFT: */
      {
 	    plan_rdft *cld = (plan_rdft *) ego->cld2;
-	    cld->apply((plan *) cld, O + os, buf);
+	    cld->apply((plan *) cld, buf, buf);
      }
-     
-     /* do inverse permutation to unshuffle the output: */
+
+     /* do inverse permutation to unshuffle the output;
+	also "fold" padded outputs (if any) back to get cyclic convolution */
      A(gpower == 1);
+     A(r - 1 == rpad || rpad >= 2*(r - 1) - 1);
 #if R2HC_ONLY_CONV
-     O[os] = buf[0];
-     gpower = g = ego->ginv;
-     for (k = 1; k < (r - 1)/2; ++k, gpower = MULMOD(gpower, g, r)) {
-	  O[gpower * os] = buf[k] + buf[r - 1 - k];
+     if (rpad == r - 1) {
+	  O[os] = buf[0];
+	  gpower = g = ego->ginv;
+	  for (k = 1; k < rpad/2; ++k, gpower = MULMOD(gpower, g, r)) {
+	       O[gpower * os] = buf[k] + buf[rpad - k];
+	  }
+	  O[gpower * os] = buf[k];
+	  ++k, gpower = MULMOD(gpower, g, r);
+	  for (; k < rpad; ++k, gpower = MULMOD(gpower, g, r)) {
+	       O[gpower * os] = buf[rpad - k] - buf[k];
+	  }
      }
-     O[gpower * os] = buf[k];
-     ++k, gpower = MULMOD(gpower, g, r);
-     for (; k < r - 1; ++k, gpower = MULMOD(gpower, g, r)) {
-	  O[gpower * os] = buf[r - 1 - k] - buf[k];
+     else {
+#error FIXME: R2HC_ONLY_CONV not yet implemented for padded Rader
      }
 #else
      g = ego->ginv;
-     for (k = 0; k < r - 1; ++k, gpower = MULMOD(gpower, g, r)) {
-	  O[gpower * os] = buf[k];
+     if (rpad == r - 1) {
+	  for (k = 0; k < r - 1; ++k, gpower = MULMOD(gpower, g, r)) {
+	       O[gpower * os] = buf[k];
+	  }
+     }
+     else {
+	  for (k = 0; k < r - 1; ++k, gpower = MULMOD(gpower, g, r)) {
+	       O[gpower * os] = buf[k] + buf[k + (r - 1)];
+	  }
      }
 #endif
      A(gpower == 1);
@@ -142,30 +163,33 @@ static void apply(const plan *ego_, R *I, R *O)
      X(ifree)(buf);
 }
 
-static R *mkomega(plan *p_, int n, int ginv)
+static R *mkomega(plan *p_, int n, int npad, int ginv)
 {
      plan_rdft *p = (plan_rdft *) p_;
      R *omega;
      int i, gpower;
      trigreal scale;
 
-     if ((omega = X(rader_tl_find)(n, n, ginv, omegas))) 
+     if ((omega = X(rader_tl_find)(n, npad + 1, ginv, omegas))) 
 	  return omega;
 
-     omega = (R *)MALLOC(sizeof(R) * (n - 1), TWIDDLES);
+     omega = (R *)MALLOC(sizeof(R) * npad, TWIDDLES);
 
-     scale = n - 1.0; /* normalization for convolution */
+     scale = npad; /* normalization for convolution */
 
      for (i = 0, gpower = 1; i < n-1; ++i, gpower = MULMOD(gpower, ginv, n)) {
 	  omega[i] = (X(cos2pi)(gpower, n) + X(sin2pi)(gpower, n)) / scale;
      }
      A(gpower == 1);
 
+     for (; i < npad; ++i)
+	  omega[i] = 0.0;
+
      AWAKE(p_, 1);
      p->apply(p_, omega, omega);
      AWAKE(p_, 0);
 
-     X(rader_tl_insert)(n, n, ginv, omega, &omegas);
+     X(rader_tl_insert)(n, npad + 1, ginv, omega, &omegas);
      return omega;
 }
 
@@ -185,7 +209,7 @@ static void awake(plan *ego_, int flg)
 
      if (flg) {
 	  if (!ego->omega) 
-	       ego->omega = mkomega(ego->cld_omega,ego->n,ego->ginv);
+	       ego->omega = mkomega(ego->cld_omega,ego->n,ego->npad,ego->ginv);
      } else {
 	  free_omega(ego->omega);
 	  ego->omega = 0;
@@ -204,8 +228,8 @@ static void print(const plan *ego_, printer *p)
 {
      const P *ego = (const P *) ego_;
 
-     p->print(p, "(dht-rader-%d%ois=%oos=%(%p%)",
-              ego->n, ego->is, ego->os, ego->cld1);
+     p->print(p, "(dht-rader-%d-%d%ois=%oos=%(%p%)",
+              ego->n, ego->npad, ego->is, ego->os, ego->cld1);
      if (ego->cld2 != ego->cld1)
           p->print(p, "%(%p%)", ego->cld2);
      if (ego->cld_omega != ego->cld1 && ego->cld_omega != ego->cld2)
@@ -235,11 +259,20 @@ static int applicable(const solver *ego, const problem *p, const planner *plnr)
      return (!NO_UGLYP(plnr) && applicable0(p));
 }
 
+static int choose_transform_size(int minsz)
+{
+     static const int primes[] = { 2, 3, 5, 0 };
+     while (!X(factors_into)(minsz, primes) || minsz % 2)
+	  ++minsz;
+     return minsz;
+}
+
 static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
 {
+     const S *ego = (const S *) ego_;
      const problem_rdft *p = (const problem_rdft *) p_;
      P *pln;
-     int n;
+     int n, npad;
      int is, os;
      plan *cld1 = (plan *) 0;
      plan *cld2 = (plan *) 0;
@@ -260,23 +293,26 @@ static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
      os = p->sz->dims[0].os;
      O = p->O;
 
+     if (ego->pad)
+	  npad = choose_transform_size(2 * (n - 1) - 1);
+     else
+	  npad = n - 1;
+
      /* initial allocation for the purpose of planning */
-     buf = (R *) MALLOC(sizeof(R) * (n - 1), BUFFERS);
+     buf = (R *) MALLOC(sizeof(R) * npad, BUFFERS);
 
      cld1 = X(mkplan_d)(plnr, 
-			X(mkproblem_rdft_1_d)(X(mktensor_1d)(n - 1, 1, os),
+			X(mkproblem_rdft_1_d)(X(mktensor_1d)(npad, 1, 1),
 					      X(mktensor_1d)(1, 0, 0),
-					      buf, 
-					      O + os,
+					      buf, buf,
 					      R2HC));
      if (!cld1) goto nada;
 
      cldp =
           X(mkproblem_rdft_1_d)(
-               X(mktensor_1d)(n - 1, os, 1),
+               X(mktensor_1d)(npad, 1, 1),
                X(mktensor_1d)(1, 0, 0),
-	       O + os,
-	       buf, 
+	       buf, buf, 
 #if R2HC_ONLY_CONV
 	       R2HC
 #else
@@ -289,7 +325,7 @@ static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
      /* plan for omega */
      plnr->planner_flags |= ESTIMATE;
      cld_omega = X(mkplan_d)(plnr, 
-			     X(mkproblem_rdft_1_d)(X(mktensor_1d)(n - 1, 1, 1),
+			     X(mkproblem_rdft_1_d)(X(mktensor_1d)(npad, 1, 1),
 						   X(mktensor_1d)(1, 0, 0),
 						   buf, buf, R2HC));
      if (!cld_omega) goto nada;
@@ -304,6 +340,7 @@ static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
      pln->cld_omega = cld_omega;
      pln->omega = 0;
      pln->n = n;
+     pln->npad = npad;
      pln->is = is;
      pln->os = os;
      pln->g = X(find_generator)(n);
@@ -311,9 +348,9 @@ static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
      A(MULMOD(pln->g, pln->ginv, n) == 1);
 
      X(ops_add)(&cld1->ops, &cld2->ops, &pln->super.super.ops);
-     pln->super.super.ops.other += (n - 3) * 3 + (n - 2) * 2 + 5;
-     pln->super.super.ops.add += (n - 3) * 1;
-     pln->super.super.ops.mul += (n - 3) * 2 + 2;
+     pln->super.super.ops.other += (npad/2-1)*6 + npad + n + (n-1) * ego->pad;
+     pln->super.super.ops.add += (npad/2-1)*2 + 2 + (n-1) * ego->pad;
+     pln->super.super.ops.mul += (npad/2-1)*4 + 2 + ego->pad;
 #if R2HC_ONLY_CONV
      pln->super.super.ops.other += (n - 2) + 4;
      pln->super.super.ops.add += (n - 3) * 1 + (n - 2) * 1;
@@ -331,14 +368,16 @@ static plan *mkplan(const solver *ego_, const problem *p_, planner *plnr)
 
 /* constructors */
 
-static solver *mksolver(void)
+static solver *mksolver(int pad)
 {
      static const solver_adt sadt = { mkplan };
      S *slv = MKSOLVER(S, &sadt);
+     slv->pad = pad;
      return &(slv->super);
 }
 
 void X(dht_rader_register)(planner *p)
 {
-     REGISTER_SOLVER(p, mksolver());
+     REGISTER_SOLVER(p, mksolver(0));
+     REGISTER_SOLVER(p, mksolver(1));
 }
