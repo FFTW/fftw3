add19c2d3c32f843ff951cc227dc4ce1221fafb6
Matteo Frigo <athena@fftw.org>
2005-02-24 10:00:02 -0500

    Do tiling recursively.

--- kernel/tile2d.c
+++ kernel/tile2d.c
@@ -21,8 +21,10 @@
 /* out of place 2D copy routines */
 #include "ifftw.h"
 
+#if 0
 /* partition [0..N0] x [0..N1] into TILESZ x TILESZ blocks,
    call F on each block */
+
 void X(tile2d)(int n0l, int n0u, int n1l, int n1u, int tilesz,
 	       void (*f)(int n0l, int n0u, int n1l, int n1u, void *args),
 	       void *args)
@@ -38,4 +40,30 @@ void X(tile2d)(int n0l, int n0u, int n1l, int n1u, int tilesz,
      for (i0 = n0l; i0 < n0u - tilesz; i0 += tilesz) 
 	  f(i0, i0 + tilesz, i1, n1u, args);
      f(i0, n0u, i1, n1u, args);
+}
+#endif
+
+/* the recursive way seems better */
+
+void X(tile2d)(int n0l, int n0u, int n1l, int n1u, int tilesz,
+	       void (*f)(int n0l, int n0u, int n1l, int n1u, void *args),
+	       void *args)
+{
+     int d0, d1;
+     
+ tail:
+     d0 = n0u - n0l;
+     d1 = n1u - n1l;
+
+     if (d0 >= d1 && d0 > tilesz) {
+	  int n0m = (n0u + n0l) / 2;
+	  X(tile2d)(n0l, n0m, n1l, n1u, tilesz, f, args);
+	  n0l = n0m; goto tail;
+     } else if (/* d1 >= d0 && */ d1 > tilesz) {
+	  int n1m = (n1u + n1l) / 2;
+	  X(tile2d)(n0l, n0u, n1l, n1m, tilesz, f, args);
+	  n1l = n1m; goto tail;
+     } else {
+	  f(n0l, n0u, n1l, n1u, args);
+     }
 }
