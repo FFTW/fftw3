e769a1735dd71165677025498471db8a41271198
Matteo Frigo <athena@fftw.org>
2005-02-25 21:54:23 -0500

    ``Interesting'' switch statement.

--- kernel/cpy1d.c
+++ kernel/cpy1d.c
@@ -28,15 +28,35 @@ void X(cpy1d)(R *I, R *O, int n0, int is0, int os0, int vl)
      A(I != O);
      switch (vl) {
 	 case 1:
-	      for (; n0 > 0; --n0, I += is0, O += os0)
-		   *O = *I;
-	      break;
+	      if ((n0 & 1) || is0 != 1 || os0 != 1) {
+		   for (; n0 > 0; --n0, I += is0, O += os0)
+			*O = *I;
+		   break;
+	      }
+	      n0 /= 2; is0 = 2; os0 = 2;
+	      /* fall through */
 	 case 2:
+	      if ((n0 & 1) || is0 != 2 || os0 != 2) {
+		   for (; n0 > 0; --n0, I += is0, O += os0) {
+			R x0 = I[0];
+			R x1 = I[1];
+			O[0] = x0;
+			O[1] = x1;
+		   }
+		   break;
+	      }
+	      n0 /= 2; is0 = 4; os0 = 4;
+	      /* fall through */
+	 case 4:
 	      for (; n0 > 0; --n0, I += is0, O += os0) {
 		   R x0 = I[0];
 		   R x1 = I[1];
+		   R x2 = I[2];
+		   R x3 = I[3];
 		   O[0] = x0;
 		   O[1] = x1;
+		   O[2] = x2;
+		   O[3] = x3;
 	      }
 	      break;
 	 default:
