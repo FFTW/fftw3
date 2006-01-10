4c5e2af8af80c06734bac116adefdc9e346caa47
Steven G. Johnson <stevenj@fftw.org>
2006-01-10 18:10:32 -0500

    more Windows decorations

--- tests/bench.c
+++ tests/bench.c
@@ -728,7 +728,7 @@ void cleanup(void)
 #    ifdef FFTW_DEBUG_MALLOC
      {
 	  /* undocumented memory checker */
-	  extern void FFTW(malloc_print_minfo)(int v);
+	  FFTW_EXTERN void FFTW(malloc_print_minfo)(int v);
 	  if (verbose > 2)
 	       FFTW(malloc_print_minfo)(verbose);
      }
