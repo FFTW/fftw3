23386506c8294fc1d61bc7cfcedb0bebc4e8fe60
athena <athena@fftw.org>
2006-12-18 14:59:16 -0500

    Oops, forgot to add file [empty commit message]

---
+++ rdft/kr2c.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2003, 2006 Matteo Frigo
+ * Copyright (c) 2003, 2006 Massachusetts Institute of Technology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#include "rdft.h"
+
+void X(kr2c_register)(planner *p, kr2c codelet, const kr2c_desc *desc)
+{
+     REGISTER_SOLVER(p, X(mksolver_rdft_r2c_direct)(codelet, desc));
+     REGISTER_SOLVER(p, X(mksolver_rdft2_direct)(codelet, desc));
+}
