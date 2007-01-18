dbb33d0b51897749feff0ef26e63af7769cd4fa5
athena <athena@fftw.org>
2007-01-18 12:09:26 -0500

    Updated copyright notices [empty commit message]

--- cell/dft-direct-cell.c
+++ cell/dft-direct-cell.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2003, 2006 Matteo Frigo
- * Copyright (c) 2003, 2006 Massachusetts Institute of Technology
+ * Copyright (c) 2007 Massachusetts Institute of Technology
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
