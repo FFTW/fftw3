4d26d141fb5e018b49133b1e080acbff744d97c0
stevenj <stevenj@fftw.org>
2007-04-25 21:19:27 -0400

    re-enable heuristic in the common case where we are not compiling for Cell [empty commit message]

--- dft/rank-geq2.c
+++ dft/rank-geq2.c
@@ -109,7 +109,7 @@ static int applicable(const solver *ego_, const problem *p_,
 
      /* FIXME: this heuristic is broken on Cell, where vrank-geq1
 	is slow */
-#if 0
+#ifndef HAVE_CELL
      /* Heuristic: if the vector stride is greater than the transform
         sz, don't use (prefer to do the vector loop first with a
         vrank-geq1 plan). */
@@ -117,6 +117,8 @@ static int applicable(const solver *ego_, const problem *p_,
 	  if (p->vecsz->rnk > 0 &&
 	      X(tensor_min_stride)(p->vecsz) > X(tensor_max_index)(p->sz))
 	       return 0;
+#else
+     UNUSED(p);
 #endif
 
      return 1;
