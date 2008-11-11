ba5c08b8b8a3c0e69ba228e071d71664f72c76ba
stevenj <stevenj@fftw.org>
2008-11-10 20:21:32 -0500

    version bump to 3.2, updated copyright year [empty commit message]

--- genfft/complex.mli
+++ genfft/complex.mli
@@ -1,7 +1,7 @@
 (*
  * Copyright (c) 1997-1999 Massachusetts Institute of Technology
- * Copyright (c) 2003, 2007 Matteo Frigo
- * Copyright (c) 2003, 2007 Massachusetts Institute of Technology
+ * Copyright (c) 2003, 2007-8 Matteo Frigo
+ * Copyright (c) 2003, 2007-8 Massachusetts Institute of Technology
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- mpi/testsched.c
+++ mpi/testsched.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2003, 2007 Matteo Frigo
- * Copyright (c) 1999-2003, 2007 Massachusetts Institute of Technology
+ * Copyright (c) 2003, 2007-8 Matteo Frigo
+ * Copyright (c) 1999-2003, 2007-8 Massachusetts Institute of Technology
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
