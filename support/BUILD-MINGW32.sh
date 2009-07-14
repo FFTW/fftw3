4de43b59f0ef8a445810e2b96e746d95a63b39b1
stevenj <stevenj@fftw.org>
2009-07-14 14:19:08 -0400

    BUILD-MINGW32 script, updated Windows README [empty commit message]

--- support/BUILD-MINGW32.sh
+++ support/BUILD-MINGW32.sh
@@ -1,48 +1,48 @@
 set -e
 
-confflags="--prefix=`pwd`/mingw64 --host=x86_64-w64-mingw32 --with-gcc-arch=nocona --enable-portable-binary --disable-alloca --with-our-malloc16 --with-windows-f77-mangling --enable-shared --disable-static --enable-threads --with-combined-threads"
+confflags="--prefix=`pwd`/mingw32 --host=i586-mingw32msvc --with-gcc-arch=prescott --enable-portable-binary --with-our-malloc16 --with-windows-f77-mangling --enable-shared --disable-static --enable-threads --with-combined-threads"
 
-rm -rf mingw64
+rm -rf mingw32
 
 (
-    rm -rf double-mingw64
-    mkdir double-mingw64
-    cd double-mingw64
+    rm -rf double-mingw32
+    mkdir double-mingw32
+    cd double-mingw32
     ../configure ${confflags} --enable-sse2 && make -j4 && make install
-    cp -f tests/.libs/bench.exe `pwd`/../mingw64/bin/bench.exe
+    cp -f tests/.libs/bench.exe `pwd`/../mingw32/bin/bench.exe
 )
 
 (
-    rm -rf single-mingw64
-    mkdir single-mingw64
-    cd single-mingw64
+    rm -rf single-mingw32
+    mkdir single-mingw32
+    cd single-mingw32
     ../configure ${confflags} --enable-sse --enable-float && make -j4 && make install
-    cp -f tests/.libs/bench.exe `pwd`/../mingw64/bin/benchf.exe
+    cp -f tests/.libs/bench.exe `pwd`/../mingw32/bin/benchf.exe
 )
 
 (
-    rm -rf ldouble-mingw64
-    mkdir ldouble-mingw64
-    cd ldouble-mingw64
+    rm -rf ldouble-mingw32
+    mkdir ldouble-mingw32
+    cd ldouble-mingw32
     ../configure ${confflags} --enable-long-double && make -j4 && make install
-    cp -f tests/.libs/bench.exe `pwd`/../mingw64/bin/benchl.exe
+    cp -f tests/.libs/bench.exe `pwd`/../mingw32/bin/benchl.exe
 )
 
 (
-cd mingw64/bin
+cd mingw32/bin
 for dll in *.dll; do
     def=`basename $dll .dll`.def
     echo "LIBRARY $dll" > $def
     echo EXPORTS >> $def
-    x86_64-w64-mingw32-nm $dll | grep ' T _' | sed 's/.* T _//' | grep fftw >> $def
+    i586-mingw32msvc-nm $dll | grep ' T _' | sed 's/.* T _//' | grep fftw >> $def
 done
 )
 
-perl -pi -e 's,^ * #define FFTW_DLL,*/\n#define FFTW_DLL\n/*,' mingw64/include/fftw3.h
+perl -pi -e 's,^ * #define FFTW_DLL,*/\n#define FFTW_DLL\n/*,' mingw32/include/fftw3.h
 
 cat > README-WINDOWS <<EOF
 This .zip archive contains DLL libraries and the associated header (.h)
-and module-definition (.def) files of FFTW compiled for Win64.  It
+and module-definition (.def) files of FFTW compiled for Win32.  It
 also contains the corresponding bench.exe test/benchmark programs
 and wisdom utilities.
 
@@ -63,11 +63,11 @@ to see whether SSE/SSE2 is supported and disables the relevant code if not).
 
 They were compiled by the GNU C compiler for MinGW, specifically:
 EOF
-x86_64-w64-mingw32-gcc --version |head -1 >> README-WINDOWS
+i586-mingw32msvc-gcc --version |head -1 >> README-WINDOWS
 cp -f tests/README README-bench
 
-fftw_vers=`grep PACKAGE_VERSION double-mingw64/config.h |cut -d" " -f3 |tr -d \"`
-zip=fftw-${fftw_vers}-dll64.zip
+fftw_vers=`grep PACKAGE_VERSION double-mingw32/config.h |cut -d" " -f3 |tr -d \"`
+zip=fftw-${fftw_vers}-dll32.zip
 rm -f $zip
-zip -vj $zip mingw64/bin/*.dll mingw64/bin/*.exe
-zip -vjgl $zip mingw64/bin/*.def mingw64/include/* README COPYING COPYRIGHT NEWS README-WINDOWS README-bench
+zip -vj $zip mingw32/bin/*.dll mingw32/bin/*.exe
+zip -vjgl $zip mingw32/bin/*.def mingw32/include/* README COPYING COPYRIGHT NEWS README-WINDOWS README-bench
