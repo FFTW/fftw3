f97162a135eee43630825fccaa29b735ee284fff
athena <athena@fftw.org>
2011-06-21 13:52:20 -0400

    remove CODELET_OPTIM
    In the old 32-bit gcc-3.x days we used to play games with gcc to force
    it to produce decent code.  Now gcc has gotten smarter and it produces
    indecent code no matter what we do, so it is safe to remove these hacks.

--- api/version.c
+++ api/version.c
@@ -22,16 +22,16 @@
 #include "api.h"
 
 const char X(cc)[] = FFTW_CC;
-const char X(codelet_optim)[] = CODELET_OPTIM;
+
+/* fftw <= 3.2.2 had special compiler flags for codelets, which are
+   not used anymore.  We keep this variable around because it is part
+   of the ABI */
+const char X(codelet_optim)[] = "";
 
 const char X(version)[] = PACKAGE "-" PACKAGE_VERSION
 
 #if HAVE_FMA
    "-fma"
-#endif
-
-#if HAVE_SSE
-   "-sse"
 #endif
 
 #if HAVE_SSE2
