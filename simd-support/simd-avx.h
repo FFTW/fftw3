f76d6c1d278b34b2ccac7cff57522cab7ec90864
athena <athena@fftw.org>
2011-06-21 16:03:24 -0400

    Initial AVX256/single implementation
    This should be correct but slow.  I need to figure out how to implement
    noncontiguous loads/stores efficiently.

--- simd-support/simd-avx.h
+++ simd-support/simd-avx.h
@@ -18,13 +18,21 @@
  *
  */
 
-#if defined(FFTW_SINGLE) || defined(FFTW_LDOUBLE) || defined(FFTW_QUAD)
-#error "AVX only works in double precision"
+#if defined(FFTW_LDOUBLE) || defined(FFTW_QUAD)
+#error "AVX only works in single or double precision"
+#endif
+
+#ifdef FFTW_SINGLE
+#  define DS(d,s) s /* single-precision option */
+#  define SUFF(name) name ## s
+#else
+#  define DS(d,s) d /* double-precision option */
+#  define SUFF(name) name ## d
 #endif
 
 #define SIMD_SUFFIX  _avx  /* for renaming */
-#define VL 2            /* SIMD complex vector length */
-#define SIMD_VSTRIDE_OKA(x) ((x) == 2)
+#define VL DS(2, 4)        /* SIMD complex vector length */
+#define SIMD_VSTRIDE_OKA(x) ((x) == 2) 
 #define SIMD_STRIDE_OKPAIR SIMD_STRIDE_OK
 
 #if defined(__GNUC__) && !defined(__AVX__) /* sanity check */
@@ -33,21 +41,96 @@
 
 #include <immintrin.h>
 
-typedef __m256d V;
-#define VADD _mm256_add_pd
-#define VSUB _mm256_sub_pd
-#define VMUL _mm256_mul_pd
-#define VXOR _mm256_xor_pd
-#define VSHUFPD _mm256_shuffle_pd
-#define VDUPL(x) _mm256_unpacklo_pd(x, x)
-#define VDUPH(x) _mm256_unpackhi_pd(x, x)
+typedef DS(__m256d, __m256) V;
+#define VADD SUFF(_mm256_add_p)
+#define VSUB SUFF(_mm256_sub_p)
+#define VMUL SUFF(_mm256_mul_p)
+#define VXOR SUFF(_mm256_xor_p)
+#define VSHUF SUFF(_mm256_shuffle_p)
 
-#define VLIT(x0, x1, x2, x3) _mm256_set_pd(x0, x1, x2, x3)
-#define DVK(var, val) V var = VLIT(val, val, val, val)
+#define SHUFVALD(fp0,fp1) \
+   (((fp1) << 3) | ((fp0) << 2) | ((fp1) << 1) | ((fp0)))
+#define SHUFVALS(fp0,fp1,fp2,fp3) \
+   (((fp3) << 6) | ((fp2) << 4) | ((fp1) << 2) | ((fp0)))
+
+#define VDUPL(x) DS(_mm256_unpacklo_pd(x, x), VSHUF(x, x, SHUFVALS(0, 0, 2, 2)))
+#define VDUPH(x) DS(_mm256_unpackhi_pd(x, x), VSHUF(x, x, SHUFVALS(1, 1, 3, 3)))
+
+#define VLIT(x0, x1) DS(_mm256_set_pd(x0, x1, x0, x1), _mm256_set_ps(x0, x1, x0, x1, x0, x1, x0, x1))
+#define DVK(var, val) V var = VLIT(val, val)
 #define LDK(x) x
-#define SHUFVAL(fp0,fp1,fp2,fp3) \
-   (((fp3) << 3) | ((fp2) << 2) | ((fp1) << 1) | ((fp0)))
 
+static inline V LDA(const R *x, INT ivs, const R *aligned_like)
+{
+     (void)aligned_like; /* UNUSED */
+     (void)ivs; /* UNUSED */
+     return SUFF(_mm256_loadu_p)(x);
+}
+
+static inline void STA(R *x, V v, INT ovs, const R *aligned_like)
+{
+     (void)aligned_like; /* UNUSED */
+     (void)ovs; /* UNUSED */
+     SUFF(_mm256_storeu_p)(x, v);
+}
+
+#if FFTW_SINGLE
+union rvec {
+     R r[8];
+     V v;
+};
+
+static inline V LD(const R *x, INT ivs, const R *aligned_like)
+{
+     /* FIXME: slow */
+     union rvec r;
+     (void)aligned_like; /* UNUSED */
+     r.r[0] = x[0];
+     r.r[1] = x[1];
+     r.r[2] = x[ivs];
+     r.r[3] = x[ivs+1];
+     r.r[4] = x[2*ivs];
+     r.r[5] = x[2*ivs+1];
+     r.r[6] = x[3*ivs];
+     r.r[7] = x[3*ivs+1];
+     return r.v;
+}
+
+static inline void ST(R *x, V v, INT ovs, const R *aligned_like)
+{
+     union rvec r;
+     (void)aligned_like; /* UNUSED */
+     r.v = v;
+     x[0] = r.r[0];
+     x[1] = r.r[1];
+     x[ovs] = r.r[2];
+     x[ovs+1] = r.r[3];
+     x[2*ovs] = r.r[4];
+     x[2*ovs+1] = r.r[5];
+     x[3*ovs] = r.r[6];
+     x[3*ovs+1] = r.r[7];
+}
+
+#define STM2 ST
+#define STN2(x, v0, v1, ovs) /* nop */
+
+static inline void STM4(R *x, V v, INT ovs, const R *aligned_like)
+{
+     union rvec r;
+     (void)aligned_like; /* UNUSED */
+     r.v = v;
+     x[0*ovs] = r.r[0];
+     x[1*ovs] = r.r[1];
+     x[2*ovs] = r.r[2];
+     x[3*ovs] = r.r[3];
+     x[4*ovs] = r.r[4];
+     x[5*ovs] = r.r[5];
+     x[6*ovs] = r.r[6];
+     x[7*ovs] = r.r[7];
+}
+#define STN4(x, v0, v1, v2, v3, ovs)  /* nop */
+
+#else
 static inline __m128d LOAD128(const R *x)
 {
      /* gcc-4.6 miscompiles the combination _mm256_castpd128_pd256(LOAD128(x))
@@ -92,13 +175,6 @@ static inline V LD(const R *x, INT ivs, const R *aligned_like)
      return var;
 }
 
-static inline V LDA(const R *x, INT ivs, const R *aligned_like)
-{
-     (void)aligned_like; /* UNUSED */
-     (void)ivs; /* UNUSED */
-     return _mm256_loadu_pd(x);
-}
-
 static inline void ST(R *x, V v, INT ovs, const R *aligned_like)
 {
      (void)aligned_like; /* UNUSED */
@@ -108,12 +184,6 @@ static inline void ST(R *x, V v, INT ovs, const R *aligned_like)
      STORE128(x, _mm256_extractf128_pd(v, 0));
 }
 
-static inline void STA(R *x, V v, INT ovs, const R *aligned_like)
-{
-     (void)aligned_like; /* UNUSED */
-     (void)ovs; /* UNUSED */
-     _mm256_storeu_pd(x, v);
-}
 
 #define STM2 ST
 #define STN2(x, v0, v1, ovs) /* nop */
@@ -131,15 +201,18 @@ static inline void STN4(R *x, V v0, V v1, V v2, V v3, INT ovs)
      STA(x + 2 * ovs, _mm256_permute2f128_pd(x0, x2, 0x31), 0, 0);
      STA(x + 3 * ovs, _mm256_permute2f128_pd(x1, x3, 0x31), 0, 0);
 }
+#endif
 
 static inline V FLIP_RI(V x)
 {
-     return VSHUFPD(x, x, SHUFVAL(1, 0, 1, 0));
+     return VSHUF(x, x,
+		  DS(SHUFVALD(1, 0), 
+		     SHUFVALS(1, 0, 3, 2)));
 }
 
 static inline V VCONJ(V x)
 {
-     V pmpm = VLIT(-0.0, 0.0, -0.0, 0.0);
+     V pmpm = VLIT(-0.0, 0.0);
      return VXOR(pmpm, x);
 }
 
@@ -185,36 +258,35 @@ static inline V VZMULIJ(V tx, V sr)
 }
 
 /* twiddle storage #1: compact, slower */
-#define VTW1(v,x)  \
-  {TW_COS, v, x}, {TW_SIN, v, x}, {TW_COS, v+1, x}, {TW_SIN, v+1, x}
+#ifdef FFTW_SINGLE
+# define VTW1(v,x) {TW_CEXP, v, x}, {TW_CEXP, v+1, x}, {TW_CEXP, v+2, x}, {TW_CEXP, v+3, x}
+#else
+# define VTW1(v,x) {TW_CEXP, v, x}, {TW_CEXP, v+1, x}
+#endif
 #define TWVL1 (VL)
 
 static inline V BYTW1(const R *t, V sr)
 {
-     const V *twp = (const V *)t;
-     V tx = twp[0];
-     V tr = VDUPL(tx);
-     V ti = VDUPH(tx);
-     tr = VMUL(tr, sr);
-     sr = VBYI(sr);
-     return VADD(tr, VMUL(ti, sr));
+     return VZMUL(LDA(t, 2, t), sr);
 }
 
 static inline V BYTWJ1(const R *t, V sr)
 {
-     const V *twp = (const V *)t;
-     V tx = twp[0];
-     V tr = VDUPL(tx);
-     V ti = VDUPH(tx);
-     tr = VMUL(tr, sr);
-     sr = VBYI(sr);
-     return VSUB(tr, VMUL(ti, sr));
+     return VZMULJ(LDA(t, 2, t), sr);
 }
 
 /* twiddle storage #2: twice the space, faster (when in cache) */
-#define VTW2(v,x)							\
-  {TW_COS, v, x}, {TW_COS, v, x}, {TW_COS, v+1, x}, {TW_COS, v+1, x},	\
-  {TW_SIN, v, -x}, {TW_SIN, v, x}, {TW_SIN, v+1, -x}, {TW_SIN, v+1, x}
+#ifdef FFTW_SINGLE
+# define VTW2(v,x)							\
+   {TW_COS, v, x}, {TW_COS, v, x}, {TW_COS, v+1, x}, {TW_COS, v+1, x},	\
+   {TW_COS, v+2, x}, {TW_COS, v+2, x}, {TW_COS, v+3, x}, {TW_COS, v+3, x}, \
+   {TW_SIN, v, -x}, {TW_SIN, v, x}, {TW_SIN, v+1, -x}, {TW_SIN, v+1, x}, \
+   {TW_SIN, v+2, -x}, {TW_SIN, v+2, x}, {TW_SIN, v+3, -x}, {TW_SIN, v+3, x}
+#else
+# define VTW2(v,x)							\
+   {TW_COS, v, x}, {TW_COS, v, x}, {TW_COS, v+1, x}, {TW_COS, v+1, x},	\
+   {TW_SIN, v, -x}, {TW_SIN, v, x}, {TW_SIN, v+1, -x}, {TW_SIN, v+1, x}
+#endif
 #define TWVL2 (2 * VL)
 
 static inline V BYTW2(const R *t, V sr)
@@ -234,13 +306,21 @@ static inline V BYTWJ2(const R *t, V sr)
 }
 
 /* twiddle storage #3 */
-#define VTW3(v,x) {TW_CEXP, v, x}, {TW_CEXP, v+1, x}
-#define TWVL3 (VL)
+#define VTW3 VTW1
+#define TWVL3 TWVL1
 
 /* twiddle storage for split arrays */
-#define VTWS(v,x)							\
+#ifdef FFTW_SINGLE
+# define VTWS(v,x)							\
+  {TW_COS, v, x}, {TW_COS, v+1, x}, {TW_COS, v+2, x}, {TW_COS, v+3, x},	\
+  {TW_COS, v+4, x}, {TW_COS, v+5, x}, {TW_COS, v+6, x}, {TW_COS, v+7, x}, \
+  {TW_SIN, v, x}, {TW_SIN, v+1, x}, {TW_SIN, v+2, x}, {TW_SIN, v+3, x},	\
+  {TW_SIN, v+4, x}, {TW_SIN, v+5, x}, {TW_SIN, v+6, x}, {TW_SIN, v+7, x}
+#else
+# define VTWS(v,x)							\
   {TW_COS, v, x}, {TW_COS, v+1, x}, {TW_COS, v+2, x}, {TW_COS, v+3, x},	\
   {TW_SIN, v, x}, {TW_SIN, v+1, x}, {TW_SIN, v+2, x}, {TW_SIN, v+3, x}	
+#endif
 #define TWVLS (2 * VL)
 
 
