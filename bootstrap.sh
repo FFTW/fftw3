fc852fcdfa80fab30eac2284249686853efa2e4b
Matteo Frigo <athena@fftw.org>
2017-09-19 15:43:02 -0400

    Remove ancient paranoia
    In the '90s we used to run autoconf three times, just in case
    (because it really didn't work the first time).  "Three" was modeled
    after the "sync; sync; sync; reboot" incantation of the '80s.
    
    Hopefully we are past this by now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,7 @@ touch ChangeLog
 
 echo "PLEASE IGNORE WARNINGS AND ERRORS"
 
-# paranoia: sometimes autoconf doesn't get things right the first time
 rm -rf autom4te.cache
-autoreconf --verbose --install --symlink --force
-autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 
 rm -f config.cache
