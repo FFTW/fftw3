c3d35def0d11716beabbae9d473d2c1b6c4593af
Galen Lynch <galen.lynch@gmail.com>
2020-06-20 13:03:28 -0400

    Add basic testing of planner_nthreads
    In an attempt to verify that planner_nthreads works as intended, I have added an
    assertion in `fftw-bench.c` that `planner_nthreads` returns the nthreads just
    passed as an argument to `plan_with_nthreads`.

--- tests/fftw-bench.c
+++ tests/fftw-bench.c
@@ -125,6 +125,7 @@ void rdwisdom(void)
      if (threads_ok) {
 	  BENCH_ASSERT(FFTW(init_threads)());
 	  FFTW(plan_with_nthreads)(nthreads);
+	  BENCH_ASSERT(FFTW(planner_nthreads)() == nthreads);
           FFTW(make_planner_thread_safe)();
 #ifdef _OPENMP
 	  omp_set_num_threads(nthreads);
